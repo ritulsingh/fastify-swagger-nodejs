4508b3b1dee6a9e956fdc04b12fdd6824e3b3037
ritul-mtalkz <ritul.mtalkz@gmail.com>
2023-01-02 16:41:00 +0530

    corrected the link of the document

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const PORT = 5000
 
 fastify.register(require('@fastify/swagger'), {
     exposeRoute: true,
-    routePrefix: "/docs",
     swagger: {
         info: {
             title: "Fastify-Api Documentation",
